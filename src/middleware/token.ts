5df025e9cbfc20330574672ad253d9a30dc70dbc
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-04-02 20:34:12 -0400

    Added more checks to token middleware

--- src/middleware/token.ts
+++ src/middleware/token.ts
@@ -2,8 +2,24 @@ import express from 'express';
 import { decryptAndUnpackToken, jsonEncodeUTF16LE } from '@/util';
 
 function tokenMiddleware(request: express.Request, response: express.Response, next: express.NextFunction): void {
-	// * RPG Maker appends a single F character to each service token, for some reason
-	request.token = decryptAndUnpackToken(request.args.token.substring(1));
+	if (!request.token) {
+		response.send(jsonEncodeUTF16LE({
+			EndCode: 102
+		}));
+
+		return;
+	}
+
+	try {
+		// * RPG Maker appends a single F character to each service token, for some reason
+		request.token = decryptAndUnpackToken(request.args.token.substring(1));
+	} catch (error) {
+		response.send(jsonEncodeUTF16LE({
+			EndCode: 102
+		}));
+
+		return;
+	}
 
 	const expireTime: number = Math.floor((Number(request.token.expire_time) / 1000));
 
